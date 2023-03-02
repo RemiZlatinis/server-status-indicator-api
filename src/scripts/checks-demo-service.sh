7ede26043b0f4a7d79dcb5e253863021655f1993
Remi Zlatinis <remizlatinis@gmail.com>
2023-03-02 21:54:54 +0200

    Simulates a long check script.

--- src/scripts/checks-demo-service.sh
+++ src/scripts/checks-demo-service.sh
@@ -2,4 +2,5 @@
 # This is the simplest possible example 
 # You can safely change or remove this script along with it's service on services.json
 # But note that a service script must echo one of ['ok' | 'update' | 'warning' | 'failure']
+sleep 10
 sudo echo 'ok' 
